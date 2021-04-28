6c34306c94c243ae44ff23877750cc00312a963e
Abel AR <abel_john_14@hotmail.com>
2021-04-28 14:00:41 -0500

    Add three objects to the card

---
+++ src/components/card/Card.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import AsesoriaInmobiliaria from '../../images/asesoria-inmobiliaria.jpg';
+import ConsultoriaTradicional from '../../images/consultoria-tradicional.jpg';
+import ConsultoriaIntegral from '../../images/consultoria-integral.jpg';
+import FinanciamientoProcompite from '../../images/financiamiento-procompite.jpg';
+import ConsultoriaAdministrativa from '../../images/consultoria-administrativa.jpg';
+import Factoring from '../../images/factoring.jpg';
+import AsesoriaLegal from '../../images/asesoria-legal.jpg';
+import AsesoriaParaObtenerFinanciamientos from '../../images/asesoria-para-obtener-financiamientos.jpg';
+
+
+const images = {
+    AsesoriaInmobiliaria,
+    ConsultoriaTradicional,
+    ConsultoriaIntegral,
+    FinanciamientoProcompite,
+    ConsultoriaAdministrativa,
+    Factoring,
+    AsesoriaLegal,
+    AsesoriaParaObtenerFinanciamientos,
+}
+
+const cases = {
+    TituloAsesoriaInmobiliaria: 'Asesoria Inmobiliaria',
+    TituloConsultoriaTradicional: 'Consultoria Tradicional',
+    TituloConsultoriaIntegral: 'Consultoria Integral',
+    TituloFinanciamientoProcompide: 'Financiamiento Procompite',
+    TituloConsultoriaAdministrativa: 'Consultoria Administrativa',
+    TituloFactoring: 'Factoring',
+    TituloAsesoriaLegal: 'Asesoria Legal',
+    TituloAsesoriaFinanciera: 'Asesoria para obtener Financiamientos',
+}
+
+const informacion = {
+    asesoriaInmobiliaria: `Te asesoramos en todo el proceso para la obtención del financiamiento procompite en la región y/o localidad que te encuentres. Elaboramos tu plan de negocios bajo los criterios establecidos en las bases del concurso procompite.`,
+    consultoriaTradicional: `-Ten un administrador a un costo accesible:
+    El servicio consiste en tener un administrador para ordenar las finanzas y proyectar los costos. Con este servicio podrás saber la situación real de tu empresa y tener proyecciones para crecer. Te ofrecemos asesoría constante y seguimiento a tus ingresos y gastos de manera semanal, realizando 2 visitas al mes para ver de manera presencial el crecimiento de tu negocio.`,
+    consultoriaIntegral: `Nuestro servicio de Consultoría integral abarca todos los temas de tu empresa, vemos la parte legal, financiera, contable, adminstrativa, marketing y ventas. Proporcionándote un estado real sobre tu empresa y resaltando los puntos que debes mejorar para seguir creciendo. Identificamos los problemas y proponemos soluciones.`,
+    financiamientoProcompide: `Te asesoramos en todo el proceso para la obtención del financiamiento procompite en la región y/o localidad que te encuentres. Elaboramos tu plan de negocios bajo los criterios establecidos en las bases del concurso procompite.`,
+    consultoriaAdministrativa: `-Ten un administrador a un costo accesible:
+    El servicio consiste en tener un administrador para ordenar las finanzas y proyectar los costos. Con este servicio podrás saber la situación real de tu empresa y tener proyecciones para crecer. Te ofrecemos asesoría constante y seguimiento a tus ingresos y gastos de manera semanal, realizando 2 visitas al mes para ver de manera presencial el crecimiento de tu negocio.`,
+    factoring: `Nuestro Servicio de Factoring te ofrece liquidez en 48 horas.
+    Contamos con Factoring Nacional para las facturas entre empresas dentro del pais.
+    Y Factoring para Comercio Internacional para las Importaciones y Exportaciones.`,
+    asesoriaLegal: `Nuestro servicio de Asesoría legal te ayuda en:
+    -Negociacion de deudas ante Entidades Financieras, SAT, SUNAT y otros
+    -Evitar Remates Judiciales
+    -Problemas Contractuales
+    -Problemas Hipotecarios
+    -Recuperación de cartera morosa.
+    -Registro de Marca y nombre comercial
+    -Patentes`,
+    asesoriaFinanciera: `Nuestro servicio te ayuda a obtener FINANCIAMIENTO para :
+    -Leasing (vehicular, Maquinaria y Equipos).
+    -Compra de local comercial o industrial.
+    -Compra de Terreno comercial o industrial.
+    -Capital de Trabajo.`,
+}
+
+const Card = () => {
+    return (
+        <div>
+            <h1>Card</h1>
+        </div>
+    )
+}
+
+export default Card;

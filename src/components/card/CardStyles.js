091121a6ed786b9478400d7628b7650290ef589c
Abel AR <abel_john_14@hotmail.com>
2021-04-30 02:20:50 -0500

    Add Data base card and new styles

---
+++ src/components/card/CardStyles.js
@@ -0,0 +1,51 @@
+import styled from "@emotion/styled";
+
+export const ServicesStyles = styled.div``;
+
+export const FirstTitleStyles = styled.h1``;
+
+export const ContainerCardsStyles = styled.div`
+    padding: 24px 8%;
+    display: grid;
+    grid-template-columns: 33% 33% 33%;
+`;
+
+export const CardStyles = styled.div`
+    margin: 0 0 48px 0;
+    width: 88%;
+    background: red;
+    border-radius: 8px;
+`;
+
+export const BoxImageStyles = styled.div`
+    width: 100%;
+    height: 264px;
+    margin: 0;
+    border-top-left-radius: 8px;
+    border-top-right-radius: 8px;
+    img {
+        width: 100%;
+        height: 100%;
+        box-sizing: border-box;
+        border-top-left-radius: 8px;
+        border-top-right-radius: 8px;
+    }
+`;
+
+export const BodyCardStyles = styled.div`
+    margin: 0;
+    padding: 32px 48px;
+    height: 320px;
+    border-bottom-left-radius: 8px;
+    border-bottom-right-radius: 8px;
+
+    h3 {
+        margin: 0;
+    }
+`;
+
+export const InformationCardStyles = styled.div``;
+
+export const PStyles = styled.p`
+    margin: 0;
+`;

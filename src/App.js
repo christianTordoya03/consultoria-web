b7e3fe8d32fb3f0afd1de64c652534b835f5c403
Abel AR <abel_john_14@hotmail.com>
2021-05-01 23:58:23 -0500

    Add welcome component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Card from "./components/card/Card";
+import Welcome from "./components/welcome/Welcome";
 
 function App() {
   return (
     <div className="App">
+      <Welcome />
       <Card />
     </div>
   );
---
+++ src/components/welcome/Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Welcome = () => {
+    return (
+        <div>
+            <h1>bienvenidos</h1>
+            <h3>Profesionales a tu servicio</h3>
+            <button>inicio</button>
+        </div>
+    )
+}
+
+export default Welcome;
